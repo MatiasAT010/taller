2ac8939019c063699a5c7ecb0f3cd6a9d1647bb6
MatiasAT010 <matiasagudelo5a@gmail.com>
2025-04-13 15:24:00 -0500

    Update CalculadoraBasica.java

--- CalculadoraBasica.java
+++ CalculadoraBasica.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class CalculadoraBasica {
+   
 
     public static int sumar(int a, int b) {
         return a + b;
@@ -24,27 +25,46 @@ public class CalculadoraBasica {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        //Preguntarle al usuario que quiere hacer
+        System.out.println("¿Que operacion matematica quieres hacer?");
+        System.out.println("");
+        System.out.println("1. Suma");
+        System.out.println("2. Resta");
+        System.out.println("3. Multiplicacion");
+        System.out.println("4. Division");
+        System.out.println("5. Comparacion");
+        int opcion = scanner.nextInt();
+        
         System.out.print("Ingresa el primer número: ");
         int num1 = scanner.nextInt();
 
         System.out.print("Ingresa el segundo número: ");
         int num2 = scanner.nextInt();
 
-        // Realizar operaciones
-        System.out.println("Suma: " + sumar(num1, num2));
-        System.out.println("Resta: " + restar(num1, num2));
-        System.out.println("Multiplicación: " + multiplicar(num1, num2));
-        System.out.println("División: " + dividir(num1, num2));
-
-        // Comparar los números usando operadores lógicos y de comparación
-        if (num1 > num2) {
+        // Realizar operaciones segun lo que el usuario haya escogido
+        switch (opcion) {
+            case 1: int sumar;
+              System.out.println("El resultado de tu suma es: " + sumar(num1, num2));
+              break;
+            case 2: int restar;
+              System.out.println("El resultado de tu resta es: " + restar(num1, num2));
+              break;
+            case 3: int multiplicar;
+             System.out.println("El resultado de tu multiplicación es: " + multiplicar(num1, num2));
+             break;
+            case 4: int dividir;
+             System.out.println("El resultado de tu división es: " + dividir(num1, num2));
+             break;
+             // Comparar los números usando operadores lógicos y de comparación
+             case 5:  if (num1 > num2) {
             System.out.println("El primer número es mayor que el segundo.");
         } else if (num1 < num2) {
             System.out.println("El segundo número es mayor que el primero.");
         } else {
             System.out.println("Ambos números son iguales.");
         }
-        scanner.close();
+            
+        }
+       scanner.close();
     }
 }
