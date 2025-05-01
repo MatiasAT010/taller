450e58fbe9f3328f12ea50c7d154971fb3177307
MatiasAT010 <matiasagudelo5a@gmail.com>
2025-04-30 21:44:22 -0500

    Update NumerosPares.java

--- NumerosPares.java
+++ NumerosPares.java
@@ -1,19 +1,54 @@
 import java.util.Scanner;
 
-public class NumerosPares {
+public class InformacionAcademica {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Ingresa la cantidad de números a evaluar: ");
-        int n = scanner.nextInt();
+        System.out.print("Ingresa tu nombre: ");
+        String nombre = scanner.nextLine();
 
-        for (int i = 1; i <= n; i++) {
-            if (i % 2 == 0) {
-                System.out.println("El número " + i + " es par.");
-            } else {
-                System.out.println("El número " + i + " es impar.");
-            }
+        System.out.print("Ingresa tu edad: ");
+        int edad = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Ingresa tu carrera: ");
+        String carrera = scanner.nextLine();
+
+        System.out.print("¿Cuántas asignaturas registrarás? ");
+        int nMaterias = scanner.nextInt();
+        scanner.nextLine();
+
+        String[] asignaturas = new String[nMaterias];
+        int[] calificaciones = new int[nMaterias];
+
+        for (int i = 0; i < nMaterias; i++) {
+            System.out.print("Ingresa nombre de la asignatura #" + (i + 1) + ": ");
+            asignaturas[i] = scanner.nextLine();
+
+            System.out.print("Ingresa calificación de " + asignaturas[i] + ": ");
+            calificaciones[i] = scanner.nextInt();
+            scanner.nextLine();
         }
+
+        System.out.println("\nNombre: " + nombre + ", Edad: " + edad + ", Carrera: " + carrera);
+
+        int suma = 0;
+        System.out.println("\nListado de calificaciones:");
+        for (int i = 0; i < nMaterias; i++) {
+            System.out.println((i + 1) + ") " + asignaturas[i] + " = " + calificaciones[i]);
+            suma += calificaciones[i];
+        }
+
+        double promedio = (double) suma / nMaterias;
+        System.out.println("\nPromedio de calificaciones: " + promedio);
+
+        if (promedio >= 3) {
+            System.out.println("Estado: aprobado");
+        } else {
+            System.out.println("Estado: reprobado");
+        }
+
         scanner.close();
     }
 }
+
